b944c06a945ffe3094f33a3d9d9318c7d0970f3c
tony gaetani <tony.gaetani@conceptboard.com>
2015-11-16 15:20:02 +0100

    bcrypt prefix can be provided in environment

--- bcrypt.bash
+++ bcrypt.bash
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pip install bcrypt > /dev/null 2>&1 || true
-python -c "import bcrypt; import getpass; print bcrypt.hashpw(getpass.getpass(), bcrypt.gensalt(prefix=b'2a'))" 2> /dev/null
+python -c "import bcrypt; import getpass; print bcrypt.hashpw(getpass.getpass(), bcrypt.gensalt(prefix=b'${PREFIX-2a}'))" 2> /dev/null
 
